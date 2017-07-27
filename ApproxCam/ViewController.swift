c51e1505aca5b7be7f6fc722edc6979f3f409524
Shining <shining@promote.cache-dns.local>
2017-07-27 22:47:45 +0800

    able to save .dng files

--- ApproxCam/ViewController.swift
+++ ApproxCam/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 import AVFoundation
 
 class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
@@ -47,6 +48,8 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         outputObject = AVCapturePhotoOutput()
         session.addOutput(outputObject)
         
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        
         session.startRunning()
     }
     
@@ -54,18 +57,67 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         print(outputObject.availablePhotoPixelFormatTypes)  //ip7:[875704422, 875704438, 1111970369]
         print(outputObject.availableRawPhotoPixelFormatTypes)
 
-        let rawFormatType = kCVPixelFormatType_32BGRA
-        let outputSettings = AVCapturePhotoSettings(format: [kCVPixelBufferPixelFormatTypeKey as String : rawFormatType])
-        outputObject.capturePhoto(with: outputSettings, delegate: self)
+        let rawFormatType = outputObject.availableRawPhotoPixelFormatTypes.first as! OSType
+        let photoSettings = AVCapturePhotoSettings(rawPixelFormatType: rawFormatType,
+                                                   processedFormat: [AVVideoCodecKey : AVVideoCodecJPEG])
+        outputObject.capturePhoto(with: photoSettings, delegate: self)
     }
     
-    func convert(cmage:CIImage) -> UIImage
-    {
-        let context:CIContext = CIContext.init(options: nil)
-        let cgImage:CGImage = context.createCGImage(cmage, from: cmage.extent)!
-        let image:UIImage = UIImage.init(cgImage: cgImage)
-        return image
+    func getCurrentTimeWithString() -> String{
+        let now = Date()
+        let timeInterval:TimeInterval = now.timeIntervalSince1970
+        return "/" + String(timeInterval) + ".dng"
     }
+    
+    func saveRAWPlusJPEGPhotoLibrary(_ rawSampleBuffer: CMSampleBuffer,
+                                     rawPreviewSampleBuffer: CMSampleBuffer?,
+                                     photoSampleBuffer: CMSampleBuffer,
+                                     previewSampleBuffer: CMSampleBuffer?,
+                                     completionHandler: ((_ success: Bool, _ error: Error?) -> Void)?) {
+        guard let jpegData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(
+            forJPEGSampleBuffer: photoSampleBuffer,
+            previewPhotoSampleBuffer: previewSampleBuffer)
+            else {
+                print("Unable to create JPEG data.")
+                completionHandler?(false, nil)
+                return
+        }
+            
+        guard let dngData = AVCapturePhotoOutput.dngPhotoDataRepresentation(
+            forRawSampleBuffer: rawSampleBuffer,
+            previewPhotoSampleBuffer: rawPreviewSampleBuffer)
+            else {
+                print("Unable to create DNG data.")
+                completionHandler?(false, nil)
+                return
+        }
+        
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let fileName = getCurrentTimeWithString()
+        let dngFileURL = URL(string: "file://\(documentsPath + fileName)")
+        print(dngFileURL!)
+        do {
+            try dngData.write(to: dngFileURL!, options: [])
+        } catch let error as NSError {
+            print("Unable to write DNG file.")
+            completionHandler?(false, error)
+            return
+        }
+            
+        PHPhotoLibrary.shared().performChanges( {
+            let creationRequest = PHAssetCreationRequest.forAsset()
+            let creationOptions = PHAssetResourceCreationOptions()
+            //creationOptions.shouldMoveFile = true
+            creationRequest.addResource(with: .photo, data: jpegData, options: nil)
+            creationRequest.addResource(with: .alternatePhoto, fileURL: dngFileURL!, options: creationOptions)
+            },
+            completionHandler: completionHandler)
+    }
+    
+    var photoSampleBuffer: CMSampleBuffer?
+    var previewPhotoSampleBuffer: CMSampleBuffer?
+    var rawSampleBuffer: CMSampleBuffer?
+    var rawPreviewPhotoSampleBuffer: CMSampleBuffer?
     
     func capture(_ captureOutput: AVCapturePhotoOutput,
                  didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?,
@@ -74,20 +126,51 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
                  bracketSettings: AVCaptureBracketedStillImageSettings?,
                  error: Error?) {
         guard error == nil, let photoSampleBuffer = photoSampleBuffer else {
-            print("Error capturing photo: \(String(describing: error))")
+            print("Error capturing photo:\(String(describing: error))")
             return
         }
         
-        guard let buffer = CMSampleBufferGetImageBuffer(photoSampleBuffer) else {
-            print("Failed.")
-            return
-        }
-        let ciImageCache = CIImage(cvPixelBuffer: buffer)
-        let image = convert(cmage: ciImageCache)
-        captureImageView.image = image
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        print("Image saved.")
+        self.photoSampleBuffer = photoSampleBuffer
+        self.previewPhotoSampleBuffer = previewPhotoSampleBuffer
     }
     
+    func capture(_ captureOutput: AVCapturePhotoOutput,
+                 didFinishProcessingRawPhotoSampleBuffer rawSampleBuffer: CMSampleBuffer?,
+                 previewPhotoSampleBuffer: CMSampleBuffer?,
+                 resolvedSettings: AVCaptureResolvedPhotoSettings,
+                 bracketSettings: AVCaptureBracketedStillImageSettings?,
+                 error: Error?) {
+        guard error == nil, let rawSampleBuffer = rawSampleBuffer else {
+            print("Error capturing RAW photo:\(String(describing: error))")
+            return
+        }
+        
+        self.rawSampleBuffer = rawSampleBuffer
+        self.rawPreviewPhotoSampleBuffer = previewPhotoSampleBuffer
+    }
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput,
+                 didFinishCaptureForResolvedSettings resolvedSettings: AVCaptureResolvedPhotoSettings,
+                 error: Error?){
+        guard error == nil else {
+            print("Error in capture process: \(String(describing: error))")
+            return
+        }
+    
+        if let rawSampleBuffer = self.rawSampleBuffer, let photoSampleBuffer = self.photoSampleBuffer {
+            saveRAWPlusJPEGPhotoLibrary(rawSampleBuffer,
+                                    rawPreviewSampleBuffer: self.rawPreviewPhotoSampleBuffer,
+            photoSampleBuffer: photoSampleBuffer,
+            previewSampleBuffer: self.previewPhotoSampleBuffer,
+            completionHandler: { success, error in
+                if success {
+                    print("Successfully added.")
+                } else {
+                    print("Error while adding \(String(describing: error))")
+                }
+            }
+            )
+        }
+    }
 }
 
